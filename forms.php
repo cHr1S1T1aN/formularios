69735286be08ceb5c3dae468742c01898dc88557
cHr1S1T1aN <chrismorats50918@gmail.com>
2025-08-09 13:22:57 -0300

    fix:melhorando o forms

--- forms.php
+++ forms.php
@@ -3,9 +3,16 @@ var_dump($_POST);
     $nome1 = $_POST["nome"];
     $city = $_POST["cidade"];
     echo "<br>".$city."<br>";
-    if ($city){
-        echo "cidade incorreta";
-    }else{
-        echo "cidade ok";
+    try{
+    if (!empty($city)){
+         throw new Exception("Cidade ok");
     }
+        if(strlen($_POST["cidade"] < 3)){
+            throw new Exception("sua cidade tem menos de 3 caracteres!");
+    }else{
+        throw new Exception("Cidade Incorreta");
+    }
+    }catch(Exception $e){
+            echo $e -> getMessage();
+        }
 ?>
