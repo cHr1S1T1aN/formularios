d5044e4d8b203cc3f785f61e3add05a18682e219
cHr1S1T1aN <chrismorats50918@gmail.com>
2025-08-09 13:21:30 -0300

    fix:Melhorando o formulario;

--- forms.php
+++ forms.php
@@ -3,8 +3,8 @@ var_dump($_POST);
     $nome1 = $_POST["nome"];
     $city = $_POST["cidade"];
     echo "<br>".$city."<br>";
-    if ($city == "a"){
-        echo "coloque uma cidade direto";
+    if ($city){
+        echo "cidade incorreta";
     }else{
         echo "cidade ok";
     }
