c8653d744e6c421f9b0452023f557fa2949707fa
cHr1S1T1aN <chrismorats50918@gmail.com>
2025-08-09 12:35:54 -0300

    ATT: testando if e else simples;

--- forms.php
+++ forms.php
@@ -1,2 +1,11 @@
 <?php
 var_dump($_POST);
+    $nome1 = $_POST["nome"];
+    $city = $_POST["cidade"];
+    echo "<br>".$city."<br>";
+    if ($city == "a"){
+        echo "coloque uma cidade direto";
+    }else{
+        echo "cidade ok";
+    }
+?>
